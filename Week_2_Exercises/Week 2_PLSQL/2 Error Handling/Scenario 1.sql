0b2166249191f82ac66c4b638906246486e27313
srijonichakraborty <96814825+srijonichakraborty@users.noreply.github.com>
2024-08-05 13:06:49 +0530

    Update Scenario 1.sql

--- Week_2_Exercises/Week 2_PLSQL/2 Error Handling/Scenario 1.sql
+++ Week_2_Exercises/Week 2_PLSQL/2 Error Handling/Scenario 1.sql
@@ -1,14 +1,3 @@
---To show the error message, before executing the procedure, first this error_logs table is created. 
---The procedure should raise an insufficient_funds exception.
---An error message should be inserted into this table.
-
---CREATE TABLE error_logs (
---  log_time TIMESTAMP(6),
---  message VARCHAR2(4000),
---  error_code NUMBER
---);
-
-
 CREATE OR REPLACE PROCEDURE SafeTransferFunds (
     from_account_id IN NUMBER,
     to_account_id IN NUMBER,
@@ -65,6 +54,15 @@ EXCEPTION
 END;
 /
 
+--To show the error message, before executing the procedure, first this error_logs table has to be created. 
+--The procedure should raise an insufficient_funds exception. An error message should be inserted into this table.
 
---Now query the error_logs table to see if an error message has been inserted.
---Look for the message "Insufficient funds for transfer".
+--CREATE TABLE error_logs (
+--  log_time TIMESTAMP(6),
+--  message VARCHAR2(4000),
+--  error_code NUMBER
+--);
+
+--Now execute this procedure.
+
+--Now query the error_logs table for insufficient fund case to see if an error message has been inserted.
