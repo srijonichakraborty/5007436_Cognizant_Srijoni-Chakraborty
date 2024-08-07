7af56b0bbe1419d979f479da600eea9a3ae629d5
srijonichakraborty <96814825+srijonichakraborty@users.noreply.github.com>
2024-08-07 12:44:10 +0530

    Add files via upload

---
+++ Week_2_Exercises/Week 2_Spring Core_Maven/6 Configuring Beans with Annotations/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,20 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    private BookRepository bookRepository;
+
+    @Autowired
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void manageBooks() {
+        System.out.println("Managing books in the library...");
+        bookRepository.saveBook();  // Example usage of the repository
+    }
+}
