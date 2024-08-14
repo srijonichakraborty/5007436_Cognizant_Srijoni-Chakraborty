ee0a94dde6da23add2256d6ea360db57d3cf7ea5
srijonichakraborty <96814825+srijonichakraborty@users.noreply.github.com>
2024-08-14 21:00:29 +0530

    Add files via upload

---
+++ Week_3_Exercises/6 Employee Management System - Implementing Pagination and Sorting/EmployeeManagementSystem/src/main/java/com/employee/dto/EmployeeDTO.java
@@ -0,0 +1,15 @@
+package com.employee.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String departmentName;
+}
